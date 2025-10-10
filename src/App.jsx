4eb07cdc86b18ce23bc24a6dbbdc13d4b14fd384
Virendra7489 <virendramalviya748929@gmail.com>
2025-10-10 22:15:33 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from './components/Navbar/Navbar'
+import Background from './components/Background/Background'
+
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+       <Background/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Background/Background.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Background.css'
+
+const Background = () => {
+  return (
+    <div className='Background'>
+      <div className='Background-text'>
+        <h1> We Ensure better education for a better world</h1>
+        <p></p>
+      </div>
+    </div>
+  )
+}
+
+export default Background
